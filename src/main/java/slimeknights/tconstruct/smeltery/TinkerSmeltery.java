a7a060f0ae3a1dbdb64ce0f008f6dd0a7e322ebd
Alejandro Silva <alejandro.felipe@outlook.com>
2026-07-07 15:04:07 -0300

    feat(gametest): generate empty arena structure templates via datagen
    Add GameTestStructureProvider, a DataProvider emitting air-filled 5x5x5
    and 9x9x9 structure templates under structure/gametest for the upcoming
    gametests, and register it in TinkerSmeltery.gatherData.

--- src/main/java/slimeknights/tconstruct/smeltery/TinkerSmeltery.java
+++ src/main/java/slimeknights/tconstruct/smeltery/TinkerSmeltery.java
@@ -540,6 +540,7 @@ public final class TinkerSmeltery extends TinkerModule {
     PackOutput packOutput = generator.getPackOutput();
     generator.addProvider(server, new SmelteryRecipeProvider(packOutput, event.getLookupProvider()));
     generator.addProvider(server, new FluidContainerTransferProvider(packOutput));
+    generator.addProvider(server, new slimeknights.tconstruct.gametest.GameTestStructureProvider(packOutput));
   }
 
   /** Adds all relevant items to the creative tab */
